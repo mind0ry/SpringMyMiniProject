1c530d77d95b3d826ae888e3c4bb8728d18341f0
KIM MINSEOK <mindory144@gmail.com>
2025-12-18 23:07:31 +0900

    2025-12-18 로그인 기능 추가

---
+++ src/main/java/com/sist/web/mapper/MemberMapper.java
@@ -0,0 +1,18 @@
+package com.sist.web.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import com.sist.web.vo.MemberVO;
+@Mapper
+@Repository
+public interface MemberMapper {
+  @Select("SELECT COUNT(*) FROM member1 "
+		 +"WHERE id=#{id}")
+  public int memberIdCount(String id);
+  
+  @Select("SELECT id,name,sex,address,pwd FROM member1 "
+		 +"WHERE id=#{id}")
+  public MemberVO memberInfoData(String id);
+}
---
+++ src/main/java/com/sist/web/vo/CultureVO.java
@@ -0,0 +1,8 @@
+package com.sist.web.vo;
+
+import lombok.Data;
+
+@Data
+public class CultureVO {
+	private String name, cate, m_cate, main_img, com_name, runtume, phone, parking, address, age;
+}
